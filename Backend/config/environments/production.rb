5e7836e84bbcb1f1f1162f25b40a738b7c3cd9d1
Echedey Henríquez <echedeyhh1989@gmail.com>
2025-05-15 14:45:47 +0100

    configure smtp production file

--- Backend/config/environments/production.rb
+++ Backend/config/environments/production.rb
@@ -71,6 +71,14 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {
+    :protocol => "https",
+    :host => "diamayorinnova.usabi.es"
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = Rails.application.credentials.smtp[Rails.env.to_sym]
+  config.action_mailer.perform_deliveries = true
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
