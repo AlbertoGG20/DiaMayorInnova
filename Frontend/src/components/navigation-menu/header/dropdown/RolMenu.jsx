b038404fb6008c18dbd5bb158df2e04121f59332
AlbertoGG20 <albertoyt13@gmail.com>
2025-05-19 11:30:07 +0100

    Role switching fixed

--- Frontend/src/components/navigation-menu/header/dropdown/RolMenu.jsx
+++ Frontend/src/components/navigation-menu/header/dropdown/RolMenu.jsx
@@ -3,12 +3,12 @@ import { navContext } from '../../../../context/nav-menu/navMenuContext';
 
 const RolMenu = () => {
 
-  const { setAdminRol, setTeacherRol, setStudentRol } = useContext(navContext);
+  const { setAdminRol, setTeacherRol, setStudentRol, isAdmin } = useContext(navContext);
 
 
   return (
     <ul>
-      <li className="userMenu_item" tabIndex={0} onClick={setAdminRol}><a> <i className="fi fi-rr-user"></i>Admin</a></li>
+      {isAdmin && <li className="userMenu_item" tabIndex={0} onClick={setAdminRol}><a> <i className="fi fi-rr-user"></i>Admin</a></li>}
       <li className="userMenu_item" tabIndex={0} onClick={setTeacherRol}><a> <i className="fi fi-rr-user"></i>Profesor</a></li>
       <li className="userMenu_item" tabIndex={0} onClick={setStudentRol}><a> <i className="fi fi-rr-user"></i>Estudiante</a></li>
     </ul>
