68670fca4a0b78960b9d1be14ddf393bc3c203c8
Alberto Clavijo <alberto.clavijo@metodo.tech>
2025-05-20 11:12:04 +0100

    fix: Add has_many class_group dependent destroy to school_centers

--- Backend/app/models/school_center.rb
+++ Backend/app/models/school_center.rb
@@ -1,6 +1,6 @@
 class SchoolCenter < ApplicationRecord
-    has_many :users, dependent: :nullify 
-    #:destroy si quieres que al eliminar un centro se eliminen los usuarios asociados, nullify permite que queden huérfanos
+    has_many :users, dependent: :destroy
+    has_many :class_groups, dependent: :destroy
 
     validates :school_name, :address, :phone, :email, :website, :province, :code, presence: true
     validates :code, uniqueness: { message: "El código del centro ya está en uso" }
