d7a594d332632f9bf0194c1dad59e49ed068f8e1
Guanchor Ojeda <guanchor@gmail.com>
2025-06-04 18:26:16 +0100

    fix migrations

--- Backend/db/migrate/20250515125450_add_code_to_school_centers.rb
+++ Backend/db/migrate/20250515125450_add_code_to_school_centers.rb
@@ -2,5 +2,7 @@ class AddCodeToSchoolCenters < ActiveRecord::Migration[7.2]
   def change
     add_column :school_centers, :code, :string
     add_index :school_centers, :code, unique: true
+    add_index :school_centers, :school_name, unique: true
+    add_index :school_centers, :email, unique: true
   end
 end
